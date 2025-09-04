4b6ddc988088a0b33ad02d078a3570076f32b39e
newbackjava <hera@naver.com>
2025-09-04 21:59:17 +0900

    commit2

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "Spring Boot! START PAGE";
+        return "Spring Boot! START PAGE v2";
     }
 
     @GetMapping("/mysql")
