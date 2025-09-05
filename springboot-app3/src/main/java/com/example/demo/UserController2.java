39eebd8275e4f3bdc4dbb61ae78dd2bc66600517
newbackjava <hera@naver.com>
2025-09-05 14:29:40 +0900

    action2

--- springboot-app3/src/main/java/com/example/demo/UserController2.java
+++ springboot-app3/src/main/java/com/example/demo/UserController2.java
@@ -18,7 +18,7 @@ public class UserController2 {
 
     @GetMapping("/")
     public String hello() {
-        return "<body bgcolor=yellow>Friday!!!!</body>;
+        return "<body bgcolor=yellow>Sunday!!!!</body>;
     }
 
     @GetMapping("/mysql")
